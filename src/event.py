ec592f5a4e9406b83f032b6f5e0d63a38498a83b
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-27 15:22:18 -0400

    only giving a heads up when getting all links

--- src/event.py
+++ src/event.py
@@ -16,15 +16,17 @@ class Event:
     def parse_event(self, event):
         if event and 'text' in event:
             if self.bot.bot_id in event['text']:
-                self.handle_event(event['user'], event['text'].split(self.bot.bot_id)[1].strip().lower(), event['channel'])
+                self.handle_event(event['user'], event['text'].split(self.bot.bot_id)[1].strip().lower(),
+                                  event['channel'])
             # If message includes a link or attachment and doesn't include 'gist.github.com/ElBell'
             elif 'gist.github.com/ElBell' not in event and link_or_attachment(event['text']):
                 add_link(event, event['channel'])
 
     def handle_event(self, user, command, channel):
-        self.bot.slack_client.api_call("chat.postMessage", channel=channel, text='I\'m on it!', as_user=True,
-                                       unfurl_links=False, unfurl_media=False)
         if command and channel:
+            if "has joined" in command:
+                self.bot.slack_client.api_call("chat.postMessage", channel=channel, text='I\'m on it!', as_user=True,
+                                               unfurl_links=False, unfurl_media=False)
             print("Received command: " + command + " in channel: " + channel + " from user: " + user)
             response = self.command.handle_command(command, channel)
             self.bot.slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True,
